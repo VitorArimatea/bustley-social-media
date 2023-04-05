bd40f3140ee2d5e27eac7fd1e2eb8c16c631009d
Vítor Arimatéa <vitorodev@outlook.com>
2023-04-05 20:35:01 -0300

    Arrumando o hidden nos dispositivos lg e adicionando conteúdo dos itens

--- components/Layout/SidebarItem.tsx
+++ components/Layout/SidebarItem.tsx
@@ -17,21 +17,39 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
     <div className='flex flex-row items-center'>
         <div
         className='
-        flex
-        items-center
-        justify-center
-        relative
-        h-14
-        w-14
-        p-4
-        rounded-full
-        hover:bg-slate-300
-        hover:bg-opacity-10
-        cursor-pointer
-        lg-hidden
+          flex
+          items-center
+          justify-center
+          relative
+          h-14
+          w-14
+          p-4
+          rounded-full
+          hover:bg-slate-300
+          hover:bg-opacity-10
+          cursor-pointer
+          lg:hidden
         '
         >
             <Icon size={28} color="white" />
+        </div>
+        <div 
+        className='
+          items-center
+          gap-4
+          relative
+          hidden
+          lg:flex
+          p-4
+          rounded-full
+          hover:bg-slate-300
+          hover:bg-opacity-10
+          cursor-pointer
+        '>
+          <Icon size={24} color="white" />
+          <a href={href}><p className='hidden lg:block text-white text-xl'>
+            {label}
+          </p></a>
         </div>
     </div>
   );
