c0db3151740c029eaaeac71fc54686083f0028a0
Vítor Arimatéa <vitorodev@outlook.com>
2023-04-06 20:31:46 -0300

    Importando header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
+
 export default function Home() {
+  
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-white text-xl">Aqui é onde irão ficar as postagens.</h1>
-      <span className="text-blue-800 text-lg">Em desenvolvimento...</span>
-    </div>
+    <>
+      <Header label="Página inicial"  />
+    </>
   );
 };
