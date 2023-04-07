fa7dbb661a153b00cf62285eecf7b8ac3cac9925
Vítor Arimatéa <vitorodev@outlook.com>
2023-04-07 12:22:11 -0300

    Criando input

---
+++ components/Input.tsx
@@ -0,0 +1,48 @@
+interface InputProps {
+    placeholder?: string;
+    value?: string;
+    type?: string;
+    disabled?: boolean;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+
+}
+
+const Input: React.FC<InputProps> = ({
+    placeholder,
+    value,
+    type,
+    disabled,
+    onChange
+}) => {
+  return (
+    <div>
+        <input
+            disabled={disabled}
+            placeholder={placeholder}
+            onChange={onChange}
+            value={value}
+            type={type}
+
+            className="
+                w-full
+                p-4
+                text-lg
+                rounded-md
+                outline-none
+                border-2
+                border-neutral-800
+                bg-black
+                text-white
+                focus:border-sky-500
+                focus:border-2
+                transition
+                disabled:bg-neutral-900
+                disabled:opacity-70
+                disabled:cursor-not-allowed
+            "
+        />
+    </div>
+  );
+};
+
+export default Input;
