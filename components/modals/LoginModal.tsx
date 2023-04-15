287e916aa58ffe5acf27c4f97e497c07bd81e0de
Vítor Arimatéa <vitorodev@outlook.com>
2023-04-15 15:05:42 -0300

    Adicionando tipo password type no input de senha

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -52,6 +52,7 @@ const LoginModal = () => {
             />
             <Input
                 placeholder="Password"
+                type="password"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
                 disabled={isLoading}
