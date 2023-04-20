495573614679984883e3859d5327a7f27f6bcf4d
Vítor Arimatéa <vitorodev@outlook.com>
2023-04-20 09:33:37 -0300

    Criando UserPage

---
+++ pages/users/[userId].tsx
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router";
+import { RingLoader } from "react-spinners";
+
+import useUser from "@/hooks/useUser";
+
+import Header from "@/components/Header";
+import UserHero from "@/components/users/UserHero";
+import UserBio from "@/components/users/UserBio";
+
+
+const userView = () => {
+  const router = useRouter();
+  const { userId } = router.query;
+
+  const { data: fetchedUser, isLoading } = useUser(userId as string);
+
+  if (isLoading || !fetchedUser) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <RingLoader color="white" size={100} />
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Header showBackArrow label={fetchedUser?.name} />
+      <UserHero userId={userId as string} />
+      <UserBio userId={userId as string} />
+    </div>
+  );
+} 
+
+export default userView;
