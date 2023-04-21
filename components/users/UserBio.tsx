191807c44ded9d5896d90cce01b56f9056e15c41
Vítor Arimatéa <vitorodev@outlook.com>
2023-04-21 14:48:48 -0300

    Adicionando modal de edição

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -4,6 +4,7 @@ import { format } from 'date-fns';
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
+import useEditModal from '@/hooks/useEditModal';
 
 import Button from '../Button';
 
@@ -17,6 +18,8 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   const { data: currentUser} = useCurrentUser();
   const { data: fetchedUser} = useUser(userId);
 
+  const editModal = useEditModal();
+
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) {
       return null;
@@ -29,7 +32,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
     <div className='border-b-[1px] border-neutral-800 pb-4'>
       <div className='flex justify-end p-2'>
         {currentUser?.id === userId ? (
-          <Button secondary label="Edit" onClick={() => {}} />
+          <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
           <Button 
             onClick={() => {}}
@@ -49,7 +52,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
         </div>
         <div className='flex flex-col mt-4'>
           <p className='text-white'>
-
+            {fetchedUser?.bio}
           </p>
           <div className='
             flex
