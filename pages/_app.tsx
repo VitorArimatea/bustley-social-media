eb3cf1711db66d6367c39c595f8f622b774debd4
Vítor Arimatéa <vitorodev@outlook.com>
2023-04-21 14:53:51 -0300

    Importando e utilizando modal de edição

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,9 @@ import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
 
 import Layout from '../components/Layout';
-import LoginModal from '@/components/modals/LoginModal';
+import EditModal from '@/components/modals/EditModal';
 import RegisterModal from '@/components/modals/RegisterModal';
+import LoginModal from '@/components/modals/LoginModal';
 
 import '@/styles/globals.css';
 
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <RegisterModal />
       <LoginModal />
       <Layout> 
