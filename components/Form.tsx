efed7553448dae459f9be06052bf598416ece293
Vítor Arimatéa <vitorodev@outlook.com>
2023-04-27 14:35:41 -0300

    Add Mutatepost for real time comment

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,6 +2,7 @@ import useRegisterModal from "@/hooks/useRegisterModal";
 import useLoginModal from "@/hooks/useLoginModal";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import usePosts from "@/hooks/usePosts";
+import usePost from "@/hooks/usePost";
 
 import { useCallback, useState } from "react";
 import { toast } from "react-hot-toast";
@@ -26,7 +27,8 @@ const Form: React.FC<FormProps> = ({
     const loginModal = useLoginModal();
 
     const { data: currentUser } = useCurrentUser();
-    const { mutate: mutatePosts } = usePosts(postId as string);
+    const { mutate: mutatePosts } = usePosts();
+    const { mutate: mutatePost } = usePost(postId as string);
 
     const [ body, setBody ] = useState('');
     const [ isLoading, setIsLoading] = useState(false);        
@@ -45,12 +47,13 @@ const Form: React.FC<FormProps> = ({
 
             setBody('');
             mutatePosts();
+            mutatePost();
         } catch (error) {
             toast.error('Something went wrong');
         } finally {
             setIsLoading(false);
         }
-    }, [ body, mutatePosts, isComment, postId ]);
+    }, [ body, mutatePosts, isComment, postId, mutatePost ]);
 
     return (
         <div className="border-b-[1px] border-neutral-800 px-5 py-2">
